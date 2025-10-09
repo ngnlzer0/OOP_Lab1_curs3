e461428815a170ed49ffcf9a0705530d10c3baed
Ivan Levytskyi <Levitskiyivan3@gmail.com>
2025-10-09 21:55:20 +0300

    try to commit change for FreightCar

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import train.models.Locomotive;
 import train.models.PassengerCar;
 import train.models.TrainCar;
 import train.enums.WagonLevel;
+import train.models.FreightCar;
 
 public class Main {
     public static void main(String[] args )
@@ -14,6 +15,18 @@ public class Main {
         System.out.println(wagon1.toString());
 
         Locomotive loc1 = new Locomotive(2,3000,1280,60, FuelType.ELECTRICITY,240);
+
+        FreightCar Fre1 = new FreightCar(3,1200,8.5,5000,20000);
+
+        System.out.println((Fre1.toString()));
+
+        Fre1.loadCargo(3333,1333);
+
+        System.out.println((Fre1.toString()));
+
+        Fre1.unloadCargo(1000,200);
+
+        System.out.println((Fre1.toString()));
 
         System.out.println(loc1.toString());
     }
--- src/main/java/train/models/FreightCar.java
+++ src/main/java/train/models/FreightCar.java
@@ -1,10 +1,94 @@
 package train.models;
 
-public class FreightCar {
-    private double volume;
+/*class that realize freight car locomotive*/
+
+public class FreightCar extends TrainCar{
+    private double maxVolume;
     private double maxWeight;
     private double currentWeight;
     private double currentVolume;
 
+    public FreightCar(int ID, double weight, double suspension_width, double maxVolume, double maxWeight)
+    {
+        super(ID,weight,suspension_width);
+        this.maxVolume = maxVolume;
+        this.maxWeight = maxWeight;
+        this.currentVolume = 0;
+        this.currentWeight = this.getWeight();
+    }
 
+    //Getters
+    public double getVolume() {return maxVolume;}
+    public double getMaxWeight() {return maxWeight;}
+    public double getCurrentWeight() {return currentWeight;}
+    public double getCurrentVolume() {return currentVolume;}
+
+    //Setters
+
+    public void setCurrentVolume(double newVolume) {
+        if (newVolume < 0) {
+            throw new IllegalArgumentException("Volume cannot be negative");
+        }
+        if (newVolume > maxVolume) {
+            throw new IllegalArgumentException("Volume exceeds maximum capacity");
+        }
+        this.currentVolume = newVolume;
+    }
+
+    public void setCurrentWeight(double newWeight) {
+        if (newWeight < getWeight()) {
+            throw new IllegalArgumentException("Current weight cannot be less than car's own weight");
+        }
+        if (newWeight > getWeight() + maxWeight) {
+            throw new IllegalArgumentException("Current weight exceeds maximum load capacity");
+        }
+        this.currentWeight = newWeight;
+    }
+
+    public void loadCargo(double cargoWeight, double cargoVolume) {
+        if (cargoWeight < 0 || cargoVolume < 0) {
+            throw new IllegalArgumentException("Cargo weight and volume must be non-negative");
+        }
+        if (currentWeight + cargoWeight > getWeight() + maxWeight) {
+            throw new IllegalArgumentException("Overloaded: cargo weight exceeds limit");
+        }
+        if (currentVolume + cargoVolume > maxVolume) {
+            throw new IllegalArgumentException("Cargo volume exceeds capacity");
+        }
+        currentWeight += cargoWeight;
+        currentVolume += cargoVolume;
+    }
+
+    public void unloadCargo(double cargoWeight, double cargoVolume) {
+        if (cargoWeight < 0 || cargoVolume < 0) {
+            throw new IllegalArgumentException("Cargo weight and volume must be non-negative");
+        }
+        if (currentWeight - cargoWeight < getWeight()) {
+            throw new IllegalArgumentException("Cannot unload more than current cargo weight");
+        }
+        if (currentVolume - cargoVolume < 0) {
+            throw new IllegalArgumentException("Cannot unload more than current cargo volume");
+        }
+        currentWeight -= cargoWeight;
+        currentVolume -= cargoVolume;
+    }
+
+    // --- abstract methods ---
+    @Override
+    public double calculateTotalWeight() {
+        return currentWeight;
+    }
+
+    @Override
+    public int getCapacity() {
+        return (int) Math.round(maxVolume);
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "FreightCar(ID=%d, ownWeight=%.2f, currentWeight=%.2f, maxLoad=%.2f, volume=%.2f, currentVolume=%.2f)",
+                getID(), getWeight(), currentWeight, maxWeight, maxVolume, currentVolume
+        );
+    }
 }
