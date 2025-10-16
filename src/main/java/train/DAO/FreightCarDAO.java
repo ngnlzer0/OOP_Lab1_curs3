56afb54c28a01c73017db8db54f9dfc280aa45dd
Ivan Levytskyi <Levitskiyivan3@gmail.com>
2025-10-16 18:34:07 +0300

    add Dao for Passanger and Freight car

---
+++ src/main/java/train/DAO/FreightCarDAO.java
@@ -0,0 +1,55 @@
+package train.DAO;
+
+import train.models.FreightCar;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FreightCarDAO {
+
+    public void saveFreightCar(int trainId, FreightCar car) {
+        String sql = """
+            INSERT INTO freight_cars (train_id, weight, suspension_width, max_volume, max_weight, current_volume, current_weight)
+            VALUES (?, ?, ?, ?, ?, ?, ?)
+            """;
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, trainId);
+            ps.setDouble(2, car.getWeight());
+            ps.setDouble(3, car.getSuspensionWidth());
+            ps.setDouble(4, car.getVolume());
+            ps.setDouble(5, car.getMaxWeight());
+            ps.setDouble(6, car.getCurrentVolume());
+            ps.setDouble(7, car.getCurrentWeight());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<FreightCar> getFreightCarsByTrainId(int trainId) {
+        List<FreightCar> cars = new ArrayList<>();
+        String sql = "SELECT * FROM freight_cars WHERE train_id = ?";
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, trainId);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                FreightCar car = new FreightCar(
+                        rs.getInt("id"),
+                        rs.getDouble("weight"),
+                        rs.getDouble("suspension_width"),
+                        rs.getDouble("max_volume"),
+                        rs.getDouble("max_weight")
+                );
+                car.setCurrentWeight(rs.getDouble("current_weight"));
+                car.setCurrentVolume(rs.getDouble("current_volume"));
+                cars.add(car);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return cars;
+    }
+}
---
+++ src/main/java/train/DAO/PassengerCarDAO.java
@@ -0,0 +1,54 @@
+package train.DAO;
+
+import train.enums.WagonLevel;
+import train.models.PassengerCar;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PassengerCarDAO {
+
+    public void savePassengerCar(int trainId, PassengerCar car) {
+        String sql = """
+            INSERT INTO passenger_cars (train_id, weight, suspension_width, number_cities, level, current_passengers)
+            VALUES (?, ?, ?, ?, ?, ?)
+            """;
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, trainId);
+            ps.setDouble(2, car.getWeight());
+            ps.setDouble(3, car.getSuspensionWidth());
+            ps.setInt(4, car.getCapacity());
+            ps.setString(5, car.getLevel().name());
+            ps.setInt(6, car.getCurrentPassengers());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<PassengerCar> getPassengerCarsByTrainId(int trainId) {
+        List<PassengerCar> cars = new ArrayList<>();
+        String sql = "SELECT * FROM passenger_cars WHERE train_id = ?";
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, trainId);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                cars.add(new PassengerCar(
+                        rs.getInt("id"),
+                        rs.getDouble("weight"),
+                        rs.getDouble("suspension_width"),
+                        rs.getInt("number_cities"),
+                        WagonLevel.valueOf(rs.getString("level")),
+                        rs.getInt("current_passengers"),
+                        0 // luggageWeight не зберігається у таблиці, можна додати пізніше
+                ));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return cars;
+    }
+}
